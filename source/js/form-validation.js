511910eccb38f8715ed63d0ec33db8febbceb4de
arkbriz <adbrizz@gmail.com>
2026-01-10 09:35:24 +0400

    add form name field validation

--- source/js/form-validation.js
+++ source/js/form-validation.js
@@ -1,3 +1,15 @@
+// validation for name form field
+const nameInput = document.querySelector('.form__input[name="name"]');
+
+nameInput.addEventListener('input', () => {
+  nameInput.value = nameInput.value.replace(/[^A-Za-zА-Яа-яЁё \-]+/g, '');
+});
+
+nameInput.addEventListener('blur', () => {
+  nameInput.value = nameInput.value.trim().replace(/\s+/g, ' ');
+});
+
+// +7 mask for phone form field
 const phoneInput = document.querySelector('.form__input[name="phone"]');
 
 phoneInput.addEventListener('focus', () => {
@@ -21,3 +33,4 @@ phoneInput.addEventListener('blur', () => {
     phoneInput.value = '';
   }
 });
+
